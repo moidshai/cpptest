715184c182ef49cbf76154a002517998fc4351be
mosh <moidshaikh125@gmail.com>
2019-11-03 04:13:28 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+// solving edabit.com
+//125#SHRLk
+
+using namespace std;
+
+//bool lessThanOrEqualToZero(int num) {
+//    if (num <= 0){
+//        return true;
+//    }
+//    else{
+//        return false;
+//    }
+//}
+//int nextEdge(int side1, int side2) {
+//    return side1 + side2 -1;
+//}
+
+//int convert(int hours, int minutes) {
+//    return hours * 3600 + minutes * 60;
+//}
+
+bool isEqual(int num1, int num2) {
+	if (num1 == num2){
+        return true;
+        }
+    else{
+        return false;
+        }
+}
+
+int main()
+{
+    cout << " - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - " << endl<< endl<< endl<< endl;
+
+    int x = 3;
+
+    cout << isEqual(3,4);
+
+//    cout << convert(1,3);
+//    cout << nextEdge(5,7);
+//  cout << lessThanOrEqualToZero(x);
+    cout << endl<< endl<< endl<< endl << " - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - " ;
+    return 0;
+}
