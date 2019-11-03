d627573c5787d3e397fb33365c16cf3753ec8a70
moidshai <57155079+moidshai@users.noreply.github.com>
2019-11-03 21:47:39 +0530

    Update main.cpp
    updated new

--- main.cpp
+++ main.cpp
@@ -20,14 +20,134 @@ using namespace std;
 //    return hours * 3600 + minutes * 60;
 //}
 
-bool isEqual(int num1, int num2) {
-	if (num1 == num2){
-        return true;
-        }
-    else{
-        return false;
-        }
-}
+// bool isEqual(int num1, int num2) {
+// 	if (num1 == num2){
+//         return true;
+//         }
+//     else{
+//         return false;
+//         }
+// }
+
+// int addition(int a, int b) {
+//     return b + a;
+// }
+
+// function triArea(base, height) {
+//     return 0.5 * base * height;
+// }
+
+// function remainder(x, y) {
+//     return x % y;
+// }
+
+// int getFirstValue(int arr[]){
+//     return arr[0];
+// }
+
+
+// bool profitableGamble(float prob, int prize, float pay) {
+//     return prob * prize > pay;    
+// }
+
+
+// bool divisibleByFive(int num) {
+//     return !(num%5);
+// }
+
+// std::string helloName(std::string name) {
+//     std::string result = "Hello ";
+//     return result + name + "!";
+// }
+
+// bool reverse(bool boolean) {
+    // return !boolean;
+// }
+
+
+// bool divisibleByFive(int num) {
+//     return !(num%5);
+// }
+
+
+// int getLastItem(int arr[], int length) {
+//     return arr[length-1];
+// }
+
+// std::string isEvenOrOdd(int num) {
+//     // return !(num%2);
+//     if (num%2 == 0){
+//         return "even";
+//     }
+//     else{
+//         return "odd";
+//     }
+// }
+
+
+// std::string concatName(std::string firstName, std::string lastName) {
+//     return lastName + ", " + firstName;
+// }
+
+// bool andAnd(bool a, bool b){
+//     return a && b;
+// }
+
+// int flipIntBool(int baseIntBoolean) {
+//     return !baseIntBoolean;
+// }
+
+// // Compare 2 strings
+// bool comp(std::string str1, std::string str2) {
+//     return str2.length() == str1.length();
+// }
+
+// // Christmas Eve
+// bool timeForMilkAndCookies(int year, int month, int day) {
+//     return month==11 && day==24;
+// }
+
+// Yeah NOPE
+// std::string yeahNope(bool b) {
+//     return b ? "yeah" : "nope"; 
+// }
+
+// equal slices between people
+// bool equalSlices(int total, int people, int each) {
+//     return total >= people * each;
+// }
+
+// Character to ASCII
+// unsigned short ctoa(char c) {
+//     return int(c);
+// }
+
+// // All operations to check
+
+// std::string operation(int num1, int num2) {
+//     if (num1+num2 == 24) 
+//         return "added";
+//     else if (abs(num1-num2) == 24)
+//         return "subtracted"; 
+//     else if (num1*num2 == 24) 
+//         return "multiplied"; 
+//      else if (num1/num2 == 24) 
+//         return "divided";
+//     else
+//         return "invalid";
+// }
+
+
+// // calculating exponent of a num
+// // include math.h to use pow()
+// #include<math.h>
+// double calculateExponent(double num1, double num2) {
+//     return pow(num1,num2);
+// }
+
+
+
+
 
 int main()
 {
@@ -35,7 +155,14 @@ int main()
 
     int x = 3;
 
-    cout << isEqual(3,4);
+    cout << helloName("sfsdf");
+
+    // cout << operation(12, 12) << endl;
+    // cout << operation(100, 76) << endl;
+    // cout << operation(6, 4) << endl;
+    // cout << operation(528, 22) << endl;
+    // cout << operation(10, 12) << endl;
+
 
 //    cout << convert(1,3);
 //    cout << nextEdge(5,7);
